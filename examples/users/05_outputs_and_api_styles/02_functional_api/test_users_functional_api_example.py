b64438f1b8b17d551dcdc3deb0d5c53f77de7b91
Chinmay <35494249+chinmaysingh31@users.noreply.github.com>
2026-06-20 03:16:08 +0530

    Fixing examples

---
+++ examples/users/05_outputs_and_api_styles/02_functional_api/test_users_functional_api_example.py
@@ -0,0 +1,23 @@
+from pytest_bdd import given, scenario, then
+from bdd_tablex import RowTable, field, parse_table, parse_table_records
+
+
+class ExampleTable(RowTable):
+    value = field("value")
+
+
+@scenario("users.feature", "Demonstrate Functional API")
+def test_functional_api():
+    pass
+
+
+@given("the example table:", target_fixture="rows")
+def example_table(datatable):
+    return datatable
+
+
+@then("the functional api behavior is correct")
+def behavior(rows):
+    assert parse_table(ExampleTable, rows)[0].value == "example"
+    assert isinstance(parse_table_records(ExampleTable, rows)[0], ExampleTable)
+
